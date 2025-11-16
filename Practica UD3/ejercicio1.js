c75c6b876fc59ec980151db21bb70d303420d4e2
JuanMiguelAbellan <240023@fppiramide.com>
2025-11-16 14:35:56 +0100

    Delete Practica UD3 directory

--- Practica UD3/ejercicio1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", listo);
-
-function listo(){
-    alert("ya esta cargado el dom");
-
-    document.addEventListener("load", alert("Ha cargado el html y ademas los recursos"))
-    
-}
