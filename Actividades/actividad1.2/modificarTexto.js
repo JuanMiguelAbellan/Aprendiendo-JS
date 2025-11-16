1f49ab64b3ee509fd8593925e190f7ab0b8cf6be
JuanMiguelAbellan <240023@fppiramide.com>
2025-11-16 14:36:41 +0100

    Delete Actividades directory

--- Actividades/actividad1.2/modificarTexto.js
+++
@@ -1,4 +0,0 @@
-
-document.write("Hola con write")
-document.getElementById("parrafo1").innerText = "Sustituyendo el parrafo1"
-document.getElementById("parrafo2").innerText += " Añadiendo al parrafo1"
--- Actividades/practicaEventos/evento.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", añadirListener)
-
-function añadirListener(){
-    const form = document.querySelector(".formulario");
-    form.addEventListener("click", (e)=>{
-        alert("Target: "+ e.target + "This: " + this.tagName)
-    })
-}
